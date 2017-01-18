6e721fe785ceeae007d613625d233643758fcc2c
Scevolla <moyaemailpochta@gmail.com>
2017-01-18 23:53:39 +0600

    Changed card-popup

--- src/EditCard.js
+++ src/EditCard.js
@@ -2,39 +2,40 @@ import React, {Component, PropTypes} from 'react';
 import CardForm from './CardForm';
  
 class EditCard extends Component {
-    componentWillMount() {
-        let card = this.props.cards.find(
-            (card)=>card.id == this.props.params.card_id);
-        this.setState(Object.assign({}, card)/*{...card}*/);
-    }
- 
-    handleChange(field, value) {
-        this.setState({[field]: value});
-    }
- 
-    handleSubmit(e) {
-        e.preventDefault();
-        this.props.cardCallbacks.updateCard(this.state);
-        this.props.router.push('/');
-    }
- 
-    handleClose(e) {
-        this.props.history.pushState(null, '/');
-    }
- 
-    render() {
-        return (
-            <CardForm draftCard={this.state}
-                      buttonLabel="Edit Card"
-                      handleChange={this.handleChange.bind(this)}
-                      handleSubmit={this.handleSubmit.bind(this)}
-                      handleClose={this.handleClose.bind(this)}/>
-        )
-    }
+  componentWillMount() {
+    let card = this.props.cards.find(
+      (card)=>card.id == this.props.params.card_id);
+    this.setState(Object.assign({}, card)/*{...card}*/);
+  }
+
+  handleChange(field, value) {
+    this.setState({[field]: value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.cardCallbacks.updateCard(this.state);
+    this.props.router.push('/');
+  }
+
+  handleClose(e) {
+    this.props.router.push('/');
+  }
+
+  render() {
+    return (
+      <CardForm draftCard={this.state}
+        buttonLabel="Edit Card"
+        handleChange={this.handleChange.bind(this)}
+        handleSubmit={this.handleSubmit.bind(this)}
+        handleClose={this.handleClose.bind(this)}
+      />
+    )
+  }
 }
 
 EditCard.propTypes = {
-    cardCallbacks: PropTypes.object
+  cardCallbacks: PropTypes.object
 };
  
 export default EditCard;
--- src/NewCard.js
+++ src/NewCard.js
@@ -2,43 +2,44 @@ import React, {Component, PropTypes} from 'react';
 import CardForm from './CardForm'
  
 class NewCard extends Component {
-    componentWillMount() {
-        this.setState({
-            id: Date.now(),
-            title: '',
-            description: '',
-            status: 'todo',
-            color: '#c9c9c9',
-            tasks: []
-        });
-    }
- 
-    handleChange(field, value) {
-        this.setState({[field]: value});
-    }
- 
-    handleSubmit(e) {
-        e.preventDefault();
-        console.log(this.props.router);
-        this.props.cardCallbacks.addCard(this.state);
-        this.props.router.push('/');
-    }
- 
-    handleClose(e) {
-        this.props.history.pushState(null, '/');
-    }
- 
-    render() {
-        return (
-            <CardForm draftCard={this.state}
-                      buttonLabel="Create Card"
-                      handleChange={this.handleChange.bind(this)}
-                      handleSubmit={this.handleSubmit.bind(this)}
-                      handleClose={this.handleClose.bind(this)}/>
-        );
-    }
+  componentWillMount() {
+    this.setState({
+      id: Date.now(),
+      title: '',
+      description: '',
+      status: 'todo',
+      color: '#c9c9c9',
+      tasks: []
+    });
+  }
+
+  handleChange(field, value) {
+    this.setState({[field]: value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.cardCallbacks.addCard(this.state);
+    this.props.router.push('/');
+  }
+
+  handleClose(e) {
+    this.props.router.push('/');
+  }
+
+  render() {
+    return (
+      <CardForm draftCard={this.state}
+        buttonLabel="Create Card"
+        handleChange={this.handleChange.bind(this)}
+        handleSubmit={this.handleSubmit.bind(this)}
+        handleClose={this.handleClose.bind(this)}
+      />
+    );
+  }
 }
+
 NewCard.propTypes = {
-    cardCallbacks: PropTypes.object
+  cardCallbacks: PropTypes.object
 };
 export default NewCard;
